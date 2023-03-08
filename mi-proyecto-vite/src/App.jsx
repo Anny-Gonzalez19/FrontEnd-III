7c9c03264837b63663d33155fb31cacfdc0b89cd
Anny-Gonzalez19 <annygonzalezd@gmail.com>
2023-03-07 19:38:21 -0500

    Clase 4:Crear un componente usando props y fragment

--- mi-proyecto-vite/src/App.jsx
+++ mi-proyecto-vite/src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
+import FuncComponent from './components/FuncComponent'
 
 function App() {
   return (
     <>
-      <h1>Clase 3 - Front End III</h1>
-      <p>Esta es la actividad de cierre de semana, en la que se integran todos los 
-        temas vistos en las clases anteriores.</p>
-      <ul>
-        <li>Clase 1: De JavaScript Vanilla a React</li>
-        <li>Clase 2: Herramientas y primer proyecto</li>
-        <li>Clase 3: Revisión y práctica I</li>
-      </ul>
-      <img src="src\assets\react.jpg" alt="Imagen React" />
+      <FuncComponent name={'Anny'}/>
+      <FuncComponent name={'Laura'}/>
+      <FuncComponent name={'Diana'}/>
     </>
   )
 }
---
+++ mi-proyecto-vite/src/components/FuncComponent.jsx
@@ -0,0 +1,10 @@
+const FuncComponent = (props) => {
+    console.log()
+  return (
+    <>
+        <h3>Hello {props.name}</h3>
+    </>
+  )
+}
+
+export default FuncComponent
