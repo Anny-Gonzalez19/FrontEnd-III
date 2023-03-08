956fa29d34cff374521a8e137f442d2851533f81
Anny-Gonzalez19 <annygonzalezd@gmail.com>
2023-03-07 19:29:43 -0500

    Clase 3:Primer proyecto con ViteJS

---
+++ mi-proyecto-vite/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Clase 3 - Front End III</h1>
+      <p>Esta es la actividad de cierre de semana, en la que se integran todos los 
+        temas vistos en las clases anteriores.</p>
+      <ul>
+        <li>Clase 1: De JavaScript Vanilla a React</li>
+        <li>Clase 2: Herramientas y primer proyecto</li>
+        <li>Clase 3: Revisión y práctica I</li>
+      </ul>
+      <img src="src\assets\react.jpg" alt="Imagen React" />
+    </>
+  )
+}
+
+export default App
